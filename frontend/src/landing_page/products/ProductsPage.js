6e93066b31343c7bc6efd5f686917cd7657791f9
Poorva <poorva2374@gmail.com>
2024-11-25 12:01:06 +0530

    Productspage corrected

--- frontend/src/landing_page/products/ProductsPage.js
+++ frontend/src/landing_page/products/ProductsPage.js
@@ -1,8 +1,8 @@
 import React from "react";
 
 import Hero from "./Hero";
-import LeftSection from "./Leftsection";
-import RightSection from "./Rightsection";
+import LeftSection from "./LeftSection";
+import RightSection from "./RightSection";
 import Universe from "./Universe";
 
 import Navbar from "../Navbar";
@@ -12,40 +12,40 @@ function PricingPage() {
   return (
     <>
       <Hero />
-      <Leftsection
+      <LeftSection
         imageURL="media/images/kite.png"
         productName="Kite"
-        productDesription="Our ultra-fast flagship trading platform with streaming market data, advanced charts, an elegant UI, and more. Enjoy the Kite experience seamlessly on your Android and iOS devices."
+        productDescription="Our ultra-fast flagship trading platform with streaming market data, advanced charts, an elegant UI, and more. Enjoy the Kite experience seamlessly on your Android and iOS devices."
         tryDemo=""
         learnMore=""
         googlePlay=""
         appStore=""
       />
-      <Rightsection
+      <RightSection
         imageURL="media/images/console.png"
         productName="Console"
-        productDesription="The central dashboard for your Zerodha account. Gain insights into your trades and investments with in-depth reports and visualisations."
+        productDescription="The central dashboard for your Zerodha account. Gain insights into your trades and investments with in-depth reports and visualisations."
         learnMore=""
       />
-      <Leftsection
+      <LeftSection
         imageURL="media/images/coin.png"
         productName="Coin"
-        productDesription="Buy direct mutual funds online, commission-free, delivered directly to your Demat account. Enjoy the investment experience on your Android and iOS devices."
+        productDescription="Buy direct mutual funds online, commission-free, delivered directly to your Demat account. Enjoy the investment experience on your Android and iOS devices."
         tryDemo=""
         learnMore=""
         googlePlay=""
         appStore=""
       />
-      <Rightsection
+      <RightSection
         imageURL="media/images/kiteconnect.png"
         productName="Kite Connect API"
-        productDesription="Build powerful trading platforms and experiences with our super simple HTTP/JSON APIs. If you are a startup, build your investment app and showcase it to our clientbase."
+        productDescription="Build powerful trading platforms and experiences with our super simple HTTP/JSON APIs. If you are a startup, build your investment app and showcase it to our clientbase."
         learnMore=""
       />
-      <Leftsection
+      <LeftSection
         imageURL="media/images/varsity.png"
         productName="Varsity mobile"
-        productDesription="An easy to grasp, collection of stock market lessons with in-depth coverage and illustrations. Content is broken down into bite-size cards to help you learn on the go."
+        productDescription="An easy to grasp, collection of stock market lessons with in-depth coverage and illustrations. Content is broken down into bite-size cards to help you learn on the go."
         tryDemo=""
         learnMore=""
         googlePlay=""
